74df0272fa0ac901a694ae1789d4a9ef6b9f9abc
alcb7 <canbulan1849@gmail.com>
2023-06-09 19:58:53 +0300

    gorev6

--- AspNetMvcBlog/App.Web.Mvc/Controllers/HomeController.cs
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/HomeController.cs
@@ -22,6 +22,28 @@ namespace App.Web.Mvc.Controllers
         {
             return View();
         }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            return View();
+        }
+        public IActionResult Index4()
+        {
+            return View();
+        }
+        public IActionResult Index5()
+        {
+            return View();
+        }
+        public IActionResult Index6()
+        {
+            return View();
+        }
+        
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
